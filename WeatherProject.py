1b6e1e9f2c5dce2b5d033c9e78cbb940307dc2fe
justdelsxo <adeleyoung@hotmail.com>
2025-05-24 11:52:59 +0100

    1st draft - Work on error message if city or country is not recognised

---
+++ WeatherProject.py
@@ -0,0 +1,50 @@
+# Install requests in the terminal before importing the library
+# Requests is used to connect to the internet to send and receive information
+
+import requests
+
+# To use this API, sign up at https://openweathermap.org/api to access the API key.
+# Replace (api_key) below with your own key.
+# This API uses the OpenWeatherMap API to fetch current weather data based on user input. You can exclude or include as many features as youd like
+
+api_key = "5b64698ebf48aad2acf480d96990c289"
+
+user = input("Which country or city are you travelling to? ")
+
+response =  requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user}&units=metric&APPID={api_key}")
+
+country = response.json()["name"]
+weather = response.json()["weather"][0]["main"]
+temperature = round(response.json()["main"]["temp"])
+feels_like = round(response.json()["main"]["feels_like"])
+
+def outfit_recommendations(temp):
+    if temp >= 30:
+        return ["Shorts", "Lightweight T-shirt's", "Sun dresses", "Sandals"]
+    elif 20 <= temp < 30:
+        return ["Linen trousers", "Maxi Dresses", "Lightweight T-shirt's", "Sandal's"]
+    elif 10 <= temp < 20:
+        return ["Jeans", "Trouser's", "Cardigan's", "Trainer's", "Shoes"]
+    elif temp < 10:
+        return ["Winter coat's", "Jumper's", "Hat's", "Scarf's", "Gloves", "Boot's"]
+    else:
+        return None
+
+outfits = outfit_recommendations(feels_like)
+
+print(f"Weather type: {weather[:6]}")
+print(f"🌤️The weather in {country} is: {temperature}°C but feels like {feels_like}°C")
+print(f"🧥 Recommended clothing to pack for {country}'s weather:")
+for item in outfits:
+    print(f"- {item}")
+
+is_warm = temperature >= 20
+is_ok = temperature > 12 < 20
+
+
+if is_warm:
+    print("Oh!, Dont forget your SPF 50, sunglasses & a fan.")
+elif is_ok:
+    print("Dont forget to bring a light jacket for the evenings, as it can get chilly!")
+else:
+    print("Wrap up warm, its a little on the cold side!")
