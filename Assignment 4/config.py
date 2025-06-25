7b1319e8d95b68c8edcdab7883f0b8b1e14ccc09
Adele. Y <adeleyoung@hotmail.com>
2025-06-25 20:08:17 +0100

    Update config.py
    Removed secrets from config file

--- Assignment 4/config.py
+++ Assignment 4/config.py
@@ -1,6 +1,6 @@
 HOST = 'localhost'
 USER = 'root'
-PASSWORD = 'Berrytruck76.' # NOTE TO SELF - REMEMBER TO REPLACE THIS WITH DUMMY DATA!!!!
+PASSWORD = '<password>' 
 DB_NAME = 'affirmations_db'
 
 
